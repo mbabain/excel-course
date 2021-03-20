937e92a6569056be8dc713182fada304b7c93af7
Maxim Babain <63829447+mbabain@users.noreply.github.com>
2021-03-20 17:56:11 +0300

    Update createStore.js

--- src/core/createStore.js
+++ src/core/createStore.js
@@ -20,26 +20,3 @@ export function createStore(rootReducer, initialState = {}) {
     },
   }
 }
-
-// export class Store {
-//   constructor(rootReducer, initialState = {}) {
-//     this.state = rootReducer({...initialState}, {type: '__INIT__'})
-//     this.listeners = []
-//   }
-
-//   subscribe(fn) {
-//     listeners.push(fn)
-//     return {
-//       unsubscribe() {
-//         listeners = listeners.filter(listener => listener !== fn)
-//       },
-//     }
-//   }
-//   dispatch(action) {
-//     state = rootReducer(state, action)
-//     listeners.forEach(listener => listener(state))
-//   }
-//   getState() {
-//     return state
-//   }
-// }
